bdde5ea6ce31341511929f43f73a7ada8658df6a
Travis Clarke <travis.m.clarke@gmail.com>
2018-10-08 21:53:50 -0700

    add resources directory

---
+++ resources/rollup-config-es.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import stripBanner from "rollup-plugin-strip-banner";
+import resolve from "rollup-plugin-node-resolve";
+
+const copyright = fs.readFileSync(path.join("resources", "COPYRIGHT"), "utf-8");
+
+const SRC_DIR = path.resolve("src");
+const DIST_DIR = path.resolve("dist");
+
+export default {
+  input: path.join(SRC_DIR, "sagamonitor.js"),
+  output: {
+    name: "Sagamonitor",
+    banner: copyright,
+    file: path.join(DIST_DIR, "sagamonitor.es.js"),
+    format: "es"
+  },
+  plugins: [
+    json(),
+    stripBanner(),
+    babel({
+      exclude: "**/node_modules/**",
+      babelrc: false
+    }),
+    resolve(),
+    commonjs()
+  ]
+};
---
+++ resources/rollup-config.js
@@ -0,0 +1,52 @@
+import fs from "fs";
+import path from "path";
+import { minify } from "uglify-js";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import saveLicense from "uglify-save-license";
+import stripBanner from "rollup-plugin-strip-banner";
+import resolve from "rollup-plugin-node-resolve";
+
+const copyright = fs.readFileSync(path.join("resources", "COPYRIGHT"), "utf-8");
+
+const SRC_DIR = path.resolve("src");
+const DIST_DIR = path.resolve("dist");
+
+export default {
+  input: path.join(SRC_DIR, "sagamonitor.js"),
+  output: {
+    name: "Sagamonitor",
+    banner: copyright,
+    exports: "named",
+    file: path.join(DIST_DIR, "sagamonitor.js"),
+    format: "umd"
+  },
+  plugins: [
+    json(),
+    stripBanner(),
+    babel({
+      exclude: "**/node_modules/**",
+      babelrc: false
+    }),
+    resolve(),
+    commonjs(),
+    {
+      name: "uglify",
+      transformBundle(code) {
+        const result = minify(code, {
+          fromString: true,
+          mangle: { toplevel: true },
+          output: { max_line_len: 2048, comments: saveLicense },
+          compress: { comparisons: true, pure_getters: true, unsafe: true }
+        });
+
+        if (!fs.existsSync(DIST_DIR)) {
+          fs.mkdirSync(DIST_DIR);
+        }
+
+        fs.writeFileSync(path.join(DIST_DIR, "sagamonitor.min.js"), result.code, "utf8");
+      }
+    }
+  ]
+};
